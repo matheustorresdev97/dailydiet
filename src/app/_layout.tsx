32e1e0368b9db014207e5c54e89e88eda500f0b7
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-06 18:09:52 -0300

    config: nativewind, font e styles

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,22 @@
+import "@/styles/global.css"
 import { Slot } from "expo-router"
+import {
+    useFonts,
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+} from '@expo-google-fonts/nunito-sans'
+import { ActivityIndicator } from "react-native"
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        NunitoSans_400Regular,
+        NunitoSans_700Bold,
+    })
+
+    if (!fontsLoaded) {
+        return <ActivityIndicator />
+    }
+
     return (
         <>
             <Slot />
---
+++ src/styles/fontFamily.ts
@@ -0,0 +1,4 @@
+export const fontFamily = {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold',
+}
